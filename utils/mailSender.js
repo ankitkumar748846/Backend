80207802c4c55d0f6a721126887a1704722f8ff4
ankit <ankitkumar748846@gmail.com>
2024-01-05 02:47:16 +0530

    first

---
+++ utils/mailSender.js
@@ -0,0 +1,13 @@
+const nodemailer = require("nodemailer");
+
+const mailSender = async (email,title,body)=>{
+    try
+    {
+        let transporter = nodemailer.createTransport
+
+    }
+    catch(error)
+    {
+        console.log(error.message);
+    }
+}
